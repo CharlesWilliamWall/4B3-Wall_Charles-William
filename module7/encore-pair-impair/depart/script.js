5dc66ae35ea7644b80491a2d874adb0ece49a529
CharlesWilliamWall <c.william.wall@gmail.com>
2022-08-30 18:32:19 -0400

    pair/impairdone

--- module7/encore-pair-impair/depart/script.js
+++ module7/encore-pair-impair/depart/script.js
@@ -1,10 +1,18 @@
 function pairImpair(input) {
+    input = window.prompt("Entrez un nombre");
+    const texte = document.getElementsByTagName('p');
 if (input == null || input == String || input != parseInt(input)) {
+    for(let r of texte){r.classList.add('red');}
     console.log("erreur");
+    document.getElementById("reponse").innerHTML = "erreur";
 }
 if(input % 2 == 0){
+    for(let t of texte){t.classList.add('blue');}
     console.log("pair");
+    document.getElementById("reponse").innerHTML = "pair";
 }if(input % 2 == 1){
+    for(let y of texte){y.classList.add('brown');}
     console.log("impair");
+    document.getElementById("reponse").innerHTML = "impair";
 }
 }
