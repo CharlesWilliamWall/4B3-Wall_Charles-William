3b5334c7b7deae00691e075001074f8a6ce6cebd
CharlesWilliamWall <c.william.wall@gmail.com>
2022-08-31 16:06:54 -0400

    correction ex1&2

--- module7/encore-pair-impair/depart/script.js
+++ module7/encore-pair-impair/depart/script.js
@@ -1,18 +1,34 @@
-function pairImpair(input) {
-    input = window.prompt("Entrez un nombre");
-    const texte = document.getElementsByTagName('p');
-if (input == null || input == String || input != parseInt(input)) {
-    for(let r of texte){r.classList.add('red');}
-    console.log("erreur");
-    document.getElementById("reponse").innerHTML = "erreur";
-}
-if(input % 2 == 0){
-    for(let t of texte){t.classList.add('blue');}
-    console.log("pair");
-    document.getElementById("reponse").innerHTML = "pair";
-}if(input % 2 == 1){
-    for(let y of texte){y.classList.add('brown');}
-    console.log("impair");
-    document.getElementById("reponse").innerHTML = "impair";
+// function pairImpair(input) {
+//     input = window.prompt("Entrez un nombre");
+//     const texte = document.getElementsByTagName('p');
+// if (input == null || input == String || input != parseInt(input)) {
+//     for(let r of texte){
+//         r.classList.add('red');
+//         console.log("erreur");
+//         document.getElementById("reponse").innerHTML = "erreur";
+//     }
+
+// }
+// if(input % 2 == 0){
+//     for(let t of texte){t.classList.add('blue');}
+//     console.log("pair");
+//     document.getElementById("reponse").innerHTML = "pair";
+// }if(input % 2 == 1){
+//     for(let y of texte){y.classList.add('brown');}
+//     console.log("impair");
+//     document.getElementById("reponse").innerHTML = "impair";
+// }
+// }
+function pairImpair(){
+    const entierStr = prompt("Entrez un nombre");
+    let entier = Number(entierStr);
+    const p = document.getElementById("reponse");
+    if ( entierStr && !isNaN(entierStr) && Number.isInteger(entier)) {
+    const pair = entier % 2 == 0 ? "pair" : "impair";
+    const rep = `Lentier ${entierStr} est <span class="${pair}">${pair}</span>.`;
+p.innerHTML = rep;
+    }
+    else {p.innerHTML = `${entierStr} n'est pas un nombre entier.`;
+    p.className = "red";
 }
 }
--- module7/j-ai-chaud/depart/script.js
+++ module7/j-ai-chaud/depart/script.js
@@ -1,25 +1,11 @@
-// function supprimerVilles(input) {
-//     const table = document.getElementById("temperature-juillet");
-//     if (! isNaN(Number(input)) && table != undefined ) {
-//     for (e in table.rows) {
-//         if(table.rows[e].cells!= undefined){
-//             if (Number(table.rows[e].cells[1].innerHTML) < Number(input)) {
-//                table.deleteRow(e);
-//             }
-//         }
-//     }
-// }
-// }
-function supprimerVilles(input) {
+function supprimerVilles(tempatureMinMax) {
     const table = document.getElementById("temperature-juillet");
-   
-    for (e in table.rows) {
-        
-        if(table.rows[e].cells != undefined){
-            if (Number(table.rows[e].cells[1].innerHTML) < Number(input)) {
-                // table.deleteRow(e);
-               console.log(table.rows[e].cells[1].innerHTML);
+
+    for (const ville of table.rows) {
+        let min = Number(ville.cells[1].innerHTML);
+            if (!isNaN(min) && min > tempatureMinMax) {
+            //    console.log(table.rows[e].cells[1].innerHTML);
+               ville.remove();
             }
         }
-    }
-    }
+}
