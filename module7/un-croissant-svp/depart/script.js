ba686f8572aa203d096415508554fccab87acbd9
CharlesWilliamWall <c.william.wall@gmail.com>
2022-09-03 15:48:02 -0400

    croissant

---
+++ module7/un-croissant-svp/depart/script.js
@@ -0,0 +1,25 @@
+function ajouterNombre() {
+    const nombreInt = prompt("Entrez votre nombre");
+    let nombre = Number(nombreInt);
+    let contenu = document.getElementById("contenu");
+    let newUl = document.createElement("ul");
+    let newLi = document.createElement("li");
+
+    if (!isNaN(nombre)) {
+        newLi.innerHTML = nombre;
+        contenu.appendChild(newUl);
+        newUl.appendChild(newLi);
+
+        let li = document.getElementsByTagName("li");
+        let liArray = Array.from(li);
+        liArray.sort(function (a, b) {
+            return a.innerHTML - b.innerHTML;
+        });
+
+        for (let i = 0; i < liArray.length; i++) {
+            newUl.appendChild(liArray[i]);
+        }
+    } else {
+        alert("Entrez un nombre valide");
+    }
+}
